0b043ffe2a8b07df421f8b25528b21449b4b7c9a
Codegamer31 <117388632+Codegamer31@users.noreply.github.com>
2024-03-18 23:12:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var len = [
+            document.getElementById(1),
+            document.getElementById(2),
+            document.getElementById(3),
+            document.getElementById(4),
+            document.getElementById(5),
+            document.getElementById(6),
+            document.getElementById(7),
+                                        ]
+
+for (let i = 0; i < len.length; i++) {
+    var element = len[i]
+    var x_index = Math.floor(Math.random()*(90-10)+10)
+    var z_index = Math.floor(Math.random()*(90-10)+10)
+    element.style.left = x_index + "vw"
+    element.style.top = z_index + "vh"
+}
