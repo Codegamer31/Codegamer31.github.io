ccc573cca46d1178d91edfa4b7da2ebb601d9c8f
Codegamer31 <117388632+Codegamer31@users.noreply.github.com>
2024-03-20 18:30:55 -0300

    Delete Açaí Pocket directory

--- Açaí Pocket/redirect-select.js
+++
@@ -1,6 +0,0 @@
-function redirecionar() 
-{
-    var select = document.getElementById("opcoes");
-    var opcaoSelecionada = select.options[select.selectedIndex].value;
-    if (opcaoSelecionada != "") {window.location.href = opcaoSelecionada;}
-}
